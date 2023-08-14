c9229436728c202062a458e6a60f3349d6659f5f
RyRyRyRy321 <hryanjames100@gmail.com>
2023-08-14 09:53:27 +0800

    Add side bar and nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { FoodItemComponent } from './food-item'
 
@@ -8,41 +5,86 @@ function App() {
 
   return (
     <div className='container border border-solid max-w-max'>
-      <nav className='nav-bar h-20 border border-solid'>
-      
+      <nav className='nav-bar w-full h-20 border border-solid'>
+        <div className="h-full flex flex-row justify-between items-center">
+        
+        <img className='w-14 h-14 rounded-full ms-12 me-10 border'></img>
+        
+        <ul className='list-none flex flex-row items-center me-auto'>
+          <li>
+            <a className='m-2 font-bold' href=''>Home</a>
+          </li>
+
+          <li>
+            <a className='m-2 font-bold' href=''>Menu</a>
+          </li>  
+        </ul>
+
+        <ul className='list-none flex flex-row-reverse items-center '>
+          <li>
+            <a className='me-5 font-bold' href=''>Log In</a>
+          </li> 
+        </ul>
+
+        
+
+        </div>
+        
+        
+
+
       </nav>
 
       <div className='flex flex-row'>
+        <div className='side-bar border border-solid h-full w-2/12 '>
+          <ul className='list-none flex flex-col p-2'>
+            <li>
+              <a>Best Deals</a>
+            </li>
 
-        <div className='side-bar border border-solid h-screen w-2/12'></div>
+            <li>
+              <a>Breakfast</a>
+            </li>
+
+            <li>
+              <a>Appetizers</a>
+            </li>
+
+            <li>
+              <a>Drinks</a>
+            </li>
+          </ul>
+        </div>
+
 
         
         <main className='border border-solid w-screen'>
 
-          <div className=''></div>
           <div className="flex justify-center">
+            <div className="carousel w-11/12 border border-solid h-72 mt-5 mb-5">
 
-            <div className="w-full grid grid-cols-4 gap-5 m-2">
-                <FoodItemComponent></FoodItemComponent>
-                <FoodItemComponent></FoodItemComponent>
-                <FoodItemComponent></FoodItemComponent>
-                <FoodItemComponent></FoodItemComponent>
-
-                <FoodItemComponent></FoodItemComponent>
-                <FoodItemComponent></FoodItemComponent>
-                <FoodItemComponent></FoodItemComponent>
-                <FoodItemComponent></FoodItemComponent>
-                
-                <FoodItemComponent></FoodItemComponent>
-                <FoodItemComponent></FoodItemComponent>
-                <FoodItemComponent></FoodItemComponent>
-                <FoodItemComponent></FoodItemComponent>
             </div>
+          </div>
+
+          
+
+
+          <div className="w-full grid grid-cols-4 gap-4 p-2">
+              <FoodItemComponent></FoodItemComponent>
+              <FoodItemComponent></FoodItemComponent>
+              <FoodItemComponent></FoodItemComponent>
+              <FoodItemComponent></FoodItemComponent>
+
+              <FoodItemComponent></FoodItemComponent>
+              <FoodItemComponent></FoodItemComponent>
+              <FoodItemComponent></FoodItemComponent>
+              <FoodItemComponent></FoodItemComponent>
+
           </div>
 
 
           
-
+      
         </main>
 
       </div>
