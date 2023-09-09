7dbbd2c39da52672d663f735112f7f69acfc1256
RyRyRyRy321 <hryanjames100@gmail.com>
2023-09-09 14:58:15 +0800

    Modify login and register modal

--- src/components/AuthModals/login-modal.jsx
+++ src/components/AuthModals/login-modal.jsx
@@ -1,35 +1,66 @@
+import { BaseModal } from "../base-modal";
 
-export function LoginModal() {
+export function LoginModal({ isOpen = false, onClose = () => {console.log("Hello world")} }) {
+    
     return (
-        <div>
-            <img></img>
-            <h2>Sign In</h2>
+        <BaseModal open={isOpen} onClose={onClose} children={<LoginContent></LoginContent>}></BaseModal>
+    );
+}
 
-            <Form>
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
-
-                <button type="submit">Login</button>
-            </Form>
-
-            <span>Sign in with:</span>
-
-            <div>
-                <button>
-                    <svg></svg>
-                </button>
-
-                <button>
-                    <svg></svg>
-                </button>
-                <button>
-                    <svg></svg>
-                </button>
+function LoginContent() {
+    return (
+        <div className="flex flex-col justify-center p-2">
+            <div className="flex flex-col justify-center items-center">
+                <img className="w-16 h-16 border m-2"></img>
+                <h2 className="text-center font-bold">Sign In</h2>
             </div>
+
+            <LoginForm></LoginForm>
+
+            <div className="flex flex-col justify-center">
+                <span className="text-center">Sign in with:</span>
+
+                <div className="flex flex-row space-x-1 justify-center">
+
+                    <button>
+                        <svg className="border" width="32px" height="32px"></svg>
+                    </button>
+
+                    <button>
+                        <svg className="border" width="32px" height="32px"></svg>
+                    </button>
+                    <button>
+                        <svg className="border" width="32px" height="32px"></svg>
+                    </button>
+                </div>
+            </div>
+
+            <div className="mb-32"></div>
+
+
 
             <button>Create a New Account</button>
         </div>
+    );
+}
 
+function LoginForm(){
 
+    const defaultSubmitFunction = (event) => {
+        event.preventDefault();
+    }
+
+    
+    return (
+            <form onSubmit={defaultSubmitFunction}>
+                <div className="flex flex-col m-2">
+                    <input className="m-1 border rounded" type="email" placeholder="Email" />
+                    <input className="m-1 border rounded" type="password" placeholder="Password" />
+                </div>
+
+                <div className="flex flex-row justify-end">
+                    <button className="border rounded m-2" type="submit">Login</button>
+                </div>
+            </form>
     );
 }
--- src/components/AuthModals/register-modal.jsx
+++ src/components/AuthModals/register-modal.jsx
@@ -1,21 +1,28 @@
-export function RegisterModal(){
+import { BaseModal } from "../base-modal";
+
+export function RegisterModal() {
+    return (<></>
+    );
+}
+
+function RegisterContent() {
     return (
         <div>
             <img></img>
             <h2>Sign In</h2>
-            
+
             <Form>
 
             </Form>
 
-            <input type="email" placeholder="Email"/>
-            <input type="password" placeholder="Password"/>
-            <input type="password" placeholder="Confirm Password"/>
-            
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="password" placeholder="Confirm Password" />
 
-            <button type = "submit">Register</button>
 
-            
+            <button type="submit">Register</button>
+
+
             <span>Already have an Account?</span>
             <button>Sign In</button>
         </div>
