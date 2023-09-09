6fc232884c16d3764a325027f172477532570d68
RyRyRyRy321 <hryanjames100@gmail.com>
2023-09-09 14:57:51 +0800

    Refactor base modal

--- src/components/base-modal.jsx
+++ src/components/base-modal.jsx
@@ -1,22 +1,23 @@
 import { Dialog } from "@headlessui/react";
+import { useState } from "react";
 
-export function BaseModal({children, open, onClose}){
+export function BaseModal({ children, open = false, onClose }) {
 
-    return (
+  return (
 
     <>
-      <Dialog className="relative z-50" open = {open} onClose={onClose} >
+      <Dialog className="relative z-50" open={open} onClose={onClose} >
 
-        <div className="fixed inset-0 bg-black-200 bg-opacity-25"/>
+        <div className="fixed inset-0 bg-black-200 bg-opacity-25" />
 
         <div className='fixed inset-0 flex items-center justify-center'>
-          <Dialog.Panel className="bg-white-100 rounded-lg p-5 shadow">            
+          <Dialog.Panel className="bg-white-100 rounded-lg p-5 shadow">
             {children}
           </Dialog.Panel>
         </div>
-        
+
       </Dialog>
     </>
-        
-    );
+
+  );
 }
