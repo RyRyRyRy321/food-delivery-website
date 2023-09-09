6f26f335cbc0db3409024b6744c6c81643e60c0b
RyRyRyRy321 <hryanjames100@gmail.com>
2023-09-09 19:05:12 +0800

    Modify register modal design

--- src/components/AuthModals/register-modal.jsx
+++ src/components/AuthModals/register-modal.jsx
@@ -11,16 +11,18 @@ function RegisterContent() {
     return (
         <div className="flex flex-col justify-center p-2">
             <div className="flex flex-col justify-center items-center">
-                <img className="w-16 h-16 border m-2"></img>
+                <img className="w-16 h-16 rounded-full border m-2"></img>
                 <h2 className="text-center font-bold">Sign Up</h2>
             </div>
 
             <RegisterForm></RegisterForm>
 
             <div className="mb-32"></div>
-
-            <span>Already have an Account?</span>
-            <button>Sign In</button>
+            
+            <div className="flex flex-col justify-center items-center">
+                <span>Already have an Account?</span>
+                <button>Sign In</button>
+            </div>
         </div>
     );
 }
@@ -28,15 +30,17 @@ function RegisterContent() {
 function RegisterForm() {
     return (
         <form>
-            <div className="flex flex-col m-2">
-                <input className="m-1 border rounded" type="email" placeholder="Email" />
-                <input className="m-1 border rounded" type="password" placeholder="Password" />
-                <input className="m-1 border rounded" type="password" placeholder="Confirm Password" />
+            <div className="flex flex-col m-2 space-y-2">
+                <input className="w-64 border rounded p-2" type="email" placeholder="Email" />
+                <input className="w-64 border rounded p-2" type="password" placeholder="Password" />
+                <input className="w-64 border rounded p-2" type="password" placeholder="Confirm Password" />
+
+                <div className="flex flex-row justify-end">
+                    <button className="border rounded-2xl p-2" type="submit">Register</button>
+                </div>
             </div>
 
-            <div className="flex flex-row justify-end">
-                <button className="border rounded m-2" type="submit">Register</button>
-            </div>
+
         </form>
     );
 }
