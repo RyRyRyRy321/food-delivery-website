6aeb09b261c1c4eedfc92639d79eec7cdebd8b6c
RyRyRyRy321 <hryanjames100@gmail.com>
2023-09-11 21:08:24 +0800

    Change hover text color

--- src/components/nav-bar-list.jsx
+++ src/components/nav-bar-list.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export function NavigationBarList({name = "None", path}){
     return(
         <li>
-            <Link className='font-bold text-black-100 hover:text-black-200' to = {path}>{name}</Link>
+            <Link className='font-bold text-black-100 hover:text-black-101' to = {path}>{name}</Link>
         </li>
     );
 }
